4b615b6999d93fda713e8a2ea9eb32ef041103c7
Hein Tay Zar <heintayzarhm@gmail.com>
2023-03-09 22:04:22 +0630

    Add UI

---
+++ app/models/ability.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # if there user is not logged in, they can only read posts
+    user ||= User.new
+    can :read, Post
+
+    return unless user.present?
+
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :manage, Like, author_id: user.id
+    return unless user.admin?
+
+    can :manage, :all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
   post '/users/:user_id/posts', to: 'posts#create', as: 'create_user_post'
+  delete '/users/:user_id/posts/:post_id', to: 'posts#destroy', as: 'delete_user_post'
   post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'create_user_post_comment'
   post '/users/:user_id/posts/:post_id/likes', to: 'likes#create', as: 'create_user_post_like'
-
 end
