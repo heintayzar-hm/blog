b97a7b29ab2687e9895fdb88977284c89484c103
Hein Tay Zar <heintayzarhm@gmail.com>
2023-03-10 20:29:01 +0630

    Fix Linters

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,9 +4,9 @@ class Api::V1::ApplicationController < ActionController::API
   before_action :authenticate_user!
 
   def authenticate_user!
-    unless user_signed_in?
-      render json: { error: 'Unauthorized' }, status: :unauthorized
-    end
+    return if user_signed_in?
+
+    render json: { error: 'Unauthorized' }, status: :unauthorized
   end
 
   def user_signed_in?
@@ -25,9 +25,7 @@ class Api::V1::ApplicationController < ActionController::API
     end
   end
 
-
   def token
     request.headers['Authorization']&.split&.last
   end
-
 end
--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, only: [:create]
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
   end
 
   def create
